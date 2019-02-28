81b3b1923d417dcabaf0a035e11b33bc2d96990a
giridhar08 <giridhar0813@gmail.com>
2019-02-28 06:56:15 +0530

    Update App.java

--- Demo2/src/main/java/OrgTest2/App.java
+++ Demo2/src/main/java/OrgTest2/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        //testign cmommit
+        //testign cmommit and push
+        //testing fetch and pull
     }
 }
